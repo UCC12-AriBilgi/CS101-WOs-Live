a37d84283208d11e6294f78107d8bd2d76f50a3f
ukaracivio <umit.karacivi@outlook.com>
2024-03-07 13:36:27 +0300

    Some changes

---
+++ WO03_Girilen20YasIcindenAraliktakiniBulma/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace WO03_Girilen20YasIcindenAraliktakiniBulma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ekrandan 20 tane yaş bilgisi gireceğim
+            // Bunlardan 25-45 yaş arası kaç personel olduğunu bulan pp.
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
