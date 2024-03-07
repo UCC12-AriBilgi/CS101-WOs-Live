1f454421d8fc6b4f58f55e61152d729a19e7e97b
ukaracivio <umit.karacivi@outlook.com>
2024-03-07 14:31:06 +0300

    Save changes

---
+++ WO05_NAdetSayininKarelerininToplami/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace WO05_NAdetSayininKarelerininToplami
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Klavyeden okunacak N adet sayinin karelerinin toplamı 
+            // for ile yapılabilir..
+
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
