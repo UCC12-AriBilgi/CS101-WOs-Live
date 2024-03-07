878b0c9a2a53d03a4b73426f4d6b5e47da7d2da9
ukaracivio <umit.karacivi@outlook.com>
2024-03-07 13:40:15 +0300

    Some changes

--- WO03_Girilen20YasIcindenAraliktakiniBulma/Program.cs
+++ WO03_Girilen20YasIcindenAraliktakiniBulma/Program.cs
@@ -7,6 +7,10 @@
             // Ekrandan 20 tane yaş bilgisi gireceğim
             // Bunlardan 25-45 yaş arası kaç personel olduğunu bulan pp.
 
+            // Burada iki farklı kullanım olabilir (for/while)
+
+
+
 
 
 
