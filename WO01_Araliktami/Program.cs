6454d2587af46fa22d6a426be033ac2be4b7e30b
ukaracivio <umit.karacivi@outlook.com>
2024-03-07 11:54:49 +0300

    Some changes

--- WO01_Araliktami/Program.cs
+++ WO01_Araliktami/Program.cs
@@ -5,11 +5,28 @@
         static void Main(string[] args)
         {
             // Ekrandan girilen bir sayının 0-100 arasında olup olmadığını gösteren (arasında veya arasında değil seklinde gösteren program parçasını yazınız.
+            int sayi = 0;
 
+            do
+            {
+                Console.Clear();
 
+                Console.WriteLine("Lütfen sayınızı giriniz..");
+                sayi = Convert.ToInt32(Console.ReadLine());
 
+                if (sayi >= 0 && sayi <= 100)
+                {
+                    Console.WriteLine($"Girmiş olduğunuz {sayi} 0-100 arasındadır..");
+                }
+                else
+                {
+                    Console.WriteLine($"Girmiş olduğunuz {sayi} 0-100 arasında değildir..");
+                }
+                
+                Console.ReadKey();
+            
+            } while (true);
 
-            Console.ReadKey();
         }
     }
 }
