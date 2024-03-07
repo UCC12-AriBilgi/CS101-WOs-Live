1cd4fad96682492222c7bc7c83025527b9d4332d
ukaracivio <umit.karacivi@outlook.com>
2024-03-07 11:39:12 +0300

    Add project files.

---
+++ WO01_Araliktami/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace WO01_Araliktami
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ekrandan girilen bir sayının 0-100 arasında olup olmadığını gösteren (arasında veya arasında değil seklinde gösteren program parçasını yazınız.
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
