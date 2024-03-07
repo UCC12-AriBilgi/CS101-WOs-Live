626c2e1000bc9f2a3bdeba8851ad52e8de63d468
ukaracivio <umit.karacivi@outlook.com>
2024-03-07 14:02:11 +0300

    Some changes

--- WO03_Girilen20YasIcindenAraliktakiniBulma/Program.cs
+++ WO03_Girilen20YasIcindenAraliktakiniBulma/Program.cs
@@ -9,14 +9,56 @@
 
             // Burada iki farklı kullanım olabilir (for/while)
 
+            int yas = 0, adet = 0;
+
+            // for ile
+            for (int i1 = 1 ; i1 <=10 ; i1++)
+            {
+                Console.Write($"{i1}.yaşı giriniz : ");
+                yas=Convert.ToInt32(Console.ReadLine());
+
+                if (yas >= 25 && yas <= 33)
+                {
+                    adet++;
+                }
+            }
+
+            SonucYaz(adet);
 
 
+            // while ile
 
+            int i = 1;
+            adet = 0; // ilk haline getir
 
+            while (i <= 10 )
+            {
+                Console.Write($"{i}.yaşı giriniz : ");
+                yas = Convert.ToInt32(Console.ReadLine());
+
+                if (yas >= 25 && yas <= 33)
+                {
+                    adet++;
+                }
+
+                i++;
+            }
+
+            SonucYaz(adet);
 
 
 
             Console.ReadKey();
         }
+
+        #region Methods
+
+        private static void SonucYaz(int pAdet)
+        {
+            Console.WriteLine($"25-33 yaş aralığında {pAdet} personel vardır. ");
+        }
+
+
+        #endregion
     }
 }
