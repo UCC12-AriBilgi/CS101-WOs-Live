c679ad827c14d7c0f2de6674d97a270680ce7356
ukaracivio <umit.karacivi@outlook.com>
2024-03-07 14:11:55 +0300

    Some changes

---
+++ WO04_SifirGireneKadarToplamVeOrtalama/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace WO04_SifirGireneKadarToplamVeOrtalama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Klavyeden 0 değeri girinceye kadar girilen sayıların toplamını ve ortalamasını bulan pp.
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
