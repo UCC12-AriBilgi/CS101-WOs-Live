337468ce1fe75b214f0345f2c8b6dad8556fc2bc
ukaracivio <umit.karacivi@outlook.com>
2024-03-07 12:00:30 +0300

    Some changes

---
+++ WO02_UcSayiEnBuyukEnKucukToplamOrtalama/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace WO02_UcSayiEnBuyukEnKucukToplamOrtalama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ekrandan alınan 3 adet tamsayının en büyüğünü,en küçüğünü,toplamlarını ve ortalamasını bulan pp.
+
+
+
+
+
+            Console.ReadKey();
+        }
+    }
+}
