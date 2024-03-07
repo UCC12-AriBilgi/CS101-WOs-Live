f36e6ec201a25819669eb1758e286de65ef42949
ukaracivio <umit.karacivi@outlook.com>
2024-03-07 14:46:55 +0300

    Some changes

--- WO05_NAdetSayininKarelerininToplami/Program.cs
+++ WO05_NAdetSayininKarelerininToplami/Program.cs
@@ -6,7 +6,21 @@
         {
             // Klavyeden okunacak N adet sayinin karelerinin toplamı 
             // for ile yapılabilir..
+            int adet = 0, sayi=0;
+            double toplam = 0;
 
+            Console.Write("Kaç adet sayı gireceksin : ");
+            adet=Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= adet; i++)
+            {
+                Console.Write($"{i}.sayıyı giriniz : ");
+                sayi=Convert.ToInt32(Console.ReadLine());
+
+                toplam += Math.Pow(sayi, 123);
+            }
+
+            Console.WriteLine($"Girilen sayıların karelerinin/küp toplamı = {toplam} ");
 
 
 
